8f6f8122f733ccbb02b7302bb3196ff540d77aa0
Chad Hinds <chadrickj8@gmail.com>
2020-04-16 12:28:24 -0400

    Re-organise callbacks

--- js/interface.js
+++ js/interface.js
@@ -1,6 +1,6 @@
 // Event listeners go in this file!
-let jams = document.getElementById("myAudio")
-jams.play();
+// let jams = document.getElementById("myAudio")
+// jams.play();
 
 
 
@@ -12,7 +12,7 @@ jams.play();
 let button0 = ['Carol', 'Corona', ''];
 let button1 = ['Wiggle', 'Anime Power', 'Yeah'];
 let button2 = ['MJ', 'Bad Day', 'Just Do It'];
-let button3 = ['Superstar','Dance', 'Running'];
+let button3 = ['Superstar', 'Dance', 'Running'];
 
 let allOptions = [button0, button1, button2, button3]
 let actions = document.querySelector('.actions');
@@ -34,71 +34,84 @@ backButton.onclick = () => {
 // }
 
 // Angry Negative Bored Tough
-const enemyArray = [['Angry Angie', 'Crazy Carl'], ['Negative Nancy', 'Komplainin Karen'], ['Boring Bob', 'Brooding Brody'], ['Tiger King','Tiger King']]
 
 let randomChar = () => {
+    if(enemyArray.length = 1) {
+        // if array has only one char remaining return it and this is Tiger King
+        return new Enemy(enemyArray[0][0])
+    }
     let firstArr = Math.floor(Math.random() * (enemyArray.length - 1));
     console.log(enemyArray, enemyArray[firstArr], firstArr)
-    return new Enemy(enemyArray[firstArr][~~(Math.random() * enemyArray[firstArr].length)]);
+    let enemy = enemyArray[firstArr].splice(~~(Math.random() * enemyArray[firstArr].length), 1);
+    if(enemyArray[firstArr].length === 0) enemyArray.splice(firstArr,1);
+
+    return new Enemy(enemy);
 }
+
+function loadEnemy() {
+    // 1st call new randChar / health
+    // change dom /img /heal;th bar / music / emotes
+    // first time you load game calla loadEnemy
+}
+
 // function buttons
-
-const buttonsEventListeners = () => {
+function buttonsEventListeners () {
+    console.log('here')
     document.querySelectorAll('button').forEach((button, i) => {
-
-       
         // Meme buttons
         button.addEventListener('click', function () {
             // Reference to the text that gets displayed in the box at the bottom
-            let battleMessage = document.querySelector('.message');
-
+            console.log(i, 'inside')
             actions.innerHTML = '';
             allOptions[i].forEach((b, i) => {
                 console.log(b)
                 let myButton = document.createElement('button');
                 myButton.setAttribute('id', `button-${+i + 1}`);
                 myButton.innerHTML = b;
-                myButton.onclick = () => {
-                    playerMessage = player.playerAttack(randomChar())
-                    //PLAYER'S TURN
-                    setTimeout(() => {
-                        document.querySelector('#enemy').classList.add('char-hit')
-                    }, 1000);  
-                    setTimeout(() => {
-                        document.querySelector('#enemy').classList.remove('char-hit')
-                    }, 1500);  
-                    // Input messages
-                    battleMessage.innerText = `${playerMessage}`
-                    // THE ENEMY'S TURN
-                   let enemyAttackMessage = setTimeout(() => {
-                        battleMessage.innerText = 'The enemy attacks you!'
-                       document.querySelector('.enemy').classList.add('attack')
-                    }, 2000);
-                   
-                    setTimeout(() => {
-                        document.querySelector('#char').classList.add('char-hit')
-                        document.querySelector('.enemy').classList.remove('attack')
-                    }, 4000);
-                    // THE PLAYER'S TURN
-                    setTimeout(() => {
-                        document.querySelector('#char').classList.remove('char-hit')
-                        battleMessage.innerText = `Choose an attack`
-                    }, 6000);
-                    enemyAttack(randomChar(), button);
-                }
+                myButton.onclick = () => onClickHandler(button);
                 actions.append(myButton);
             })
             actions.appendChild(backButton)
         })
     })
-
+    
+}
+console.log('test test [][][]')
+buttonsEventListeners()
+function onClickHandler (button) {
+    let battleMessage = document.querySelector('.message');
+    //PLAYER'S TURN
+    playerMessage = player.playerAttack(randomChar())
+    // Input messages
+    battleMessage.innerText = `${playerMessage}`
+    // THE ENEMY'S TURN
+    let enemyAttackMessage = setTimeout(() => {
+        battleMessage.innerText = 'The enemy attacks you!'
+        document.querySelector('.enemy').classList.add('attack')
+    }, 2000);
+    setTimeout(() => {
+        document.querySelector('#char').classList.add('char-hit')
+        document.querySelector('.enemy').classList.remove('attack')
+    }, 4000);
+    let jams = document.getElementById("myAudio")
+    jams.volume = 0.2
+    jams.play();
+    // THE PLAYER'S TURN
+    setTimeout(() => {
+        document.querySelector('#char').classList.remove('char-hit')
+        document.querySelector('.enemy').classList.add('char-hit')
+        battleMessage.innerText = `Choose an attack`
+    }, 6000);
+    setTimeout(() => {
+        document.querySelector('.enemy').classList.remove('char-hit')
+    }, 6500);
+    enemyAttack(randomChar(), button);
 }
 
 // function gameStart() {
 //     document.querySelector("#start").addEventListener {
 //     }
 // }
-buttonsEventListeners()
 
 
 // PASS AS SECOND PARAM TO SETTIMEOUT
