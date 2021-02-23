4d9ccb1d7ebf5823238699f9b4a01d13b404b472
Chad Hinds <chadrickj8@gmail.com>
2021-02-23 12:11:53 -0500

    Inital setup of context

---
+++ lyric_finder/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="" component={Index} />
+          </Switch>
+        </div>
+      </>
+    </Router>
+  );
+}
+
+export default App;
